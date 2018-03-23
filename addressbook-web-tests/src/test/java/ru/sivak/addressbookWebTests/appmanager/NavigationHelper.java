3ad96b826c52c977ca52312b26e34f7598339930
Sivak Pavel <pavel.sivak@stoloto.ru>
2018-03-23 17:46:31 +0300

    Оптимизация clickHome, clickAddNew, clickGroups

--- addressbook-web-tests/src/test/java/ru/sivak/addressbookWebTests/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/sivak/addressbookWebTests/appmanager/NavigationHelper.java
@@ -11,14 +11,25 @@ public class NavigationHelper extends HelperBase{
     }
 
     public void clickHome() {
+        if (isElementPresent(By.id("maintable"))){
+            return;
+        }
         click(By.linkText("home"));
     }
 
     public void clickAddNew() {
+        if (wd.findElement(By.tagName("h1")).getText().equals("Edit / add address book entry")){
+            return;
+        }
         click(By.linkText("add new"));
     }
 
     public void clickGroups() {
+        if (isElementPresent(By.tagName("h1"))
+                && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+                && isElementPresent(By.name("new"))){
+            return;
+        }
         click(By.linkText("groups"));
     }
 }
