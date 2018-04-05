5c247fc41a6963df3aee1f82884b47da580f6103
Sivak Pavel <pavel.sivak@stoloto.ru>
2018-04-05 15:37:39 +0300

    Создания параметризованы через провайдер

--- addressbook-web-tests/src/test/java/ru/sivak/addressbookWebTests/tests/NewContactTest.java
+++ addressbook-web-tests/src/test/java/ru/sivak/addressbookWebTests/tests/NewContactTest.java
@@ -1,10 +1,15 @@
 package ru.sivak.addressbookWebTests.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.sivak.addressbookWebTests.model.Contacts;
 import ru.sivak.addressbookWebTests.model.NewContactParameters;
+import ru.sivak.addressbookWebTests.model.NewGroupParameters;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -12,13 +17,21 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class NewContactTest extends TestBase {
 
-    @Test
-    public void createNewContact() {
+    @DataProvider
+    public Iterator<Object[]> validContacts() {
+        List<Object[]> list = new ArrayList<>();
+        list.add(new Object[]{new NewContactParameters().withFirst("test1").withLast("header1").withAddress("footer1").withMobile("123").withHome("123").withWork("123")});
+        list.add(new Object[]{new NewContactParameters().withFirst("test2").withLast("header2").withAddress("footer2").withMobile("1234").withHome("1234").withWork("1234")});
+        list.add(new Object[]{new NewContactParameters().withFirst("test3").withLast("header3").withAddress("footer3").withMobile("1235").withHome("1235").withWork("1235")});
+        return list.iterator();
+    }
+
+    @Test(dataProvider = "validContacts")
+    public void createNewContact(NewContactParameters contact) {
         app.goTo().home();
         Contacts before = app.contact().all();
         app.goTo().addNew();
-        File photo = new File("src/test/resources/qwer.png");
-        NewContactParameters contact = new NewContactParameters().withFirst("test").withMobile("123").withHome("123").withWork("123").withPhoto(photo);
+        //File photo = new File("src/test/resources/qwer.png");
         app.contact().create(contact, true);
         Contacts after = app.contact().all();
         assertThat(after.size(), equalTo(before.size() + 1));
--- addressbook-web-tests/src/test/java/ru/sivak/addressbookWebTests/tests/NewGroupTest.java
+++ addressbook-web-tests/src/test/java/ru/sivak/addressbookWebTests/tests/NewGroupTest.java
@@ -1,8 +1,13 @@
 package ru.sivak.addressbookWebTests.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.sivak.addressbookWebTests.model.Groups;
 import ru.sivak.addressbookWebTests.model.NewGroupParameters;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -10,11 +15,19 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class NewGroupTest extends TestBase {
 
-    @Test
-    public void createNewGroup() {
+    @DataProvider
+    public Iterator<Object[]> validGroups() {
+        List<Object[]> list = new ArrayList<>();
+        list.add(new Object[]{new NewGroupParameters().withName("test1").withHead("header1").withFoot("footer1")});
+        list.add(new Object[]{new NewGroupParameters().withName("test2").withHead("header2").withFoot("footer2")});
+        list.add(new Object[]{new NewGroupParameters().withName("test3").withHead("header3").withFoot("footer3")});
+        return list.iterator();
+    }
+
+    @Test(dataProvider = "validGroups")
+    public void createNewGroup(NewGroupParameters group) {
         app.goTo().groups();
         Groups before = app.group().all();
-        NewGroupParameters group = new NewGroupParameters().withName("test");
         app.group().create(group);
         assertThat(app.groupHelper.count(), equalTo(before.size() + 1));
         Groups after = app.group().all();
