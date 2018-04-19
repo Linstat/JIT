fe315fac559418157e7e2556e119a9c0d565cc01
Sivak Pavel <pavel.sivak@stoloto.ru>
2018-04-19 13:31:56 +0300

    Создан проект для мантиса

---
+++ mantis-tests/src/test/java/ru/sivak/mantis/tests/TestBase.java
@@ -0,0 +1,39 @@
+package ru.sivak.mantis.tests;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.sivak.mantis.appmanager.ApplicationManager;
+
+import java.io.IOException;
+
+/**
+ * @author p.sivak.
+ * @since 20.03.2018.
+ */
+public class TestBase {
+
+
+    protected static ApplicationManager app;
+
+    static {
+        try {
+            app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @BeforeSuite
+    public void setUp() throws IOException {
+        app.init();
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() {
+        app.stop();
+    }
+
+
+}
